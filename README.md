967215eaf7b0ea1dae1906b590af886c08180576
Lena Brueder <oss@lena-brueder.de>
2015-12-06 19:57:49 +0100

    - added functionality to scan for the fastest kilometer, and for the fastest distance in general for a track

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# sleepy-skunk
-Sleepy Skunk is an activity tracker.
+# Sleepy Skunk
+Sleepy Skunk is an activity tracker. It analyzes sets of GPX
+files. In the end, it should be a server-side-software to get
+a fitness tracking server that gives you control over your
+own data. You should be able to add activities through a simple
+POST to the correct URL.
+
+Tracking yourself is cool, but giving away your
+data is more like 💩.
--- app/models/Track.scala
+++ app/models/Track.scala
@@ -2,18 +2,47 @@ package models
 
 import java.time.Duration
 
+import scala.language.implicitConversions
+
 /**
   * A track that consists of multiple locations.
   */
-trait Track extends Seq[Location] {
+class Track(seq: Seq[Location]) {
+  import Track.seqLocationToTrack
+
   //time must not go backwards!
-  require(seq.foldLeft((true, head.dateTime))((tuple, location) =>
+  require(seq.foldLeft((true, seq.head.dateTime))((tuple, location) =>
     (tuple._1 && !tuple._2.isBefore(location.dateTime), location.dateTime))._1)
 
-  val distance: Double = seq match {
+  lazy val distance: Double = seq match {
     case head :: tail => tail.foldLeft(head, 0.0)((accum, elem) => (elem, accum._2 + accum._1.distance(elem)))._2
     case Nil          => 0.0
   }
 
-  val averagePace = Duration.between(head.dateTime, last.dateTime).getSeconds / (distance * 1000.0)
+  lazy val averagePace = Duration.between(seq.head.dateTime, seq.last.dateTime).getSeconds / (distance * 1000.0)
+
+  /**return the fastest kilometer, start index together with duration*/
+  lazy val fastestKilometer: Option[(Int, Duration)] = fastestDistance(1000.0)
+
+  /**returns the fastest distance (in meters), start index together with duration*/
+  def fastestDistance(distance: Double): Option[(Int, Duration)] = {
+    val newSeq = (0 to seq.size).flatMap(slidingWindowOfLength(_, distance)).zipWithIndex
+    newSeq.toSeq match {
+      case Nil => None //can happen when the given distance is longer than the distance of this track
+      case list =>
+        val min = list.minBy(_._1.distance)
+        val (track, index) = min
+        Some((index, Duration.between(track.seq.head.dateTime, track.seq.last.dateTime)))
+    }
+  }
+
+  /**creates a new sub-sequence that has all points to form a given distance, if any*/
+  private def slidingWindowOfLength(startIndex: Int, length: Double): Option[Seq[Location]] = {
+    val subSeq = seq.drop(startIndex)
+    (0 to subSeq.size).map(subSeq.take).find(_.distance > length)
+  }
+}
+
+object Track {
+  implicit def seqLocationToTrack(seq: Seq[Location]): Track = new Track(seq)
 }
